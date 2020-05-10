d64d774dd98283a276962fa24d20fb73e87c8f4b
mahajant99 <mahajant99@gmail.com>
2020-05-10 23:11:51 +0530

    nuke
    Signed-off-by: mahajant99 <mahajant99@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,15 +184,6 @@ TARGET_PROVIDES_QTI_TELEPHONY_JAR := true
 # Recovery
 TARGET_RECOVERY_FSTAB := $(VENDOR_PATH)/rootdir/fstab.qcom
 
-# SELinux
-include device/qcom/sepolicy-legacy-um/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(VENDOR_PATH)/sepolicy
-
-# We modify several neverallows, so let the build proceed
-ifneq ($(TARGET_BUILD_VARIANT),user)
-SELINUX_IGNORE_NEVERALLOWS := true
-endif
-
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HOSTAPD_DRIVER := NL80211
